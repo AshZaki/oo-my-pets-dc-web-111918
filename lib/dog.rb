f31eb3b3276f712c3421b53d9f7a28256832ab02
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-30 20:34:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,17 @@ class Dog
     @name = name
     @mood = "nervous"
   end
+  
+  # attr_accessor :mood, :owner
+  # attr_reader :name
+  
+  # @@all = []
+  
+  # def initialize(name, owner)
+  #   @name = name
+  #   @mood = "nervous"
+  #   @owner = owner
+  #   @@all << self
+  # end
+  
 end
